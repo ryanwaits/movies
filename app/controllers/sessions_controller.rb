75e2e91f8881154f8e171ae78e5c65f50fa96e79
Ryan Waits <ryan.waits@gmail.com>
2016-11-04 14:03:10 -0500

    add sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_username(params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+end
