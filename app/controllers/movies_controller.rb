e569d95f8780db3ba9d3c9e5d3e56959bc9e60a3
Ryan Waits <ryan.waits@gmail.com>
2016-11-04 14:04:11 -0500

    update index and create actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.all.order('created_at DESC')
   end
 
   def show
@@ -9,8 +10,20 @@ class MoviesController < ApplicationController
   end
 
   def create
+    @movie = current_user.movies.build(movie_params)
+    if @movie.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def new
+  end
+
+  private 
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :duration, :rating)
   end
 end
