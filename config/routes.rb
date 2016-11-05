bec7a49fa5e3be23f2f15dec340b1f3ca5d74a00
Ryan Waits <ryan.waits@gmail.com>
2016-11-04 20:31:50 -0500

    nest reviews within movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+
   get 'login' => 'sessions#new', as: 'login'
   get 'logout' => 'sessions#destroy', as: 'logout'
   resources :sessions, only: [:create]
@@ -7,7 +9,9 @@ Rails.application.routes.draw do
 
   root 'movies#index'
   
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create, :destroy]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
