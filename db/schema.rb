bdc7d1f44a54f6f7a593a965c4e36318bf0187dc
Ryan Waits <ryan.waits@gmail.com>
2016-11-04 20:31:33 -0500

    create reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104180045) do
+ActiveRecord::Schema.define(version: 20161104222049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20161104180045) do
     t.index ["user_id"], name: "index_movies_on_user_id", using: :btree
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
@@ -34,4 +42,5 @@ ActiveRecord::Schema.define(version: 20161104180045) do
   end
 
   add_foreign_key "movies", "users"
+  add_foreign_key "reviews", "movies"
 end
